33a7790ed163c4b789aa1e39ea3cc7a14a4e75d6
Alejandro Zapata <alejo8708@gmail.com>
2023-03-03 13:42:40 +0100

    lab-19

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -3,64 +3,150 @@ import { SyntheticEvent, useState } from "react";
 import { Project } from "./Project";
 
 interface ProjectFormProps {
-    onCancel: () => void
-    onSave: (project: Project) => void
-    project: Project
+  onCancel: () => void;
+  onSave: (project: Project) => void;
+  project: Project;
 }
 
-function ProjectForm({ onCancel, onSave, project: initialProject }: ProjectFormProps) {
+function ProjectForm({
+  onCancel,
+  onSave,
+  project: initialProject,
+}: ProjectFormProps) {
+  const [project, setProject] = useState(initialProject);
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+    budget: "",
+  });
 
-  const [project, setProject] = useState(initialProject)
 
   const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault()
-    onSave(project)
-  }
+    event.preventDefault();
+    if (!isValid()) return;
+    onSave(project);
+  };
+
 
   const handleChange = (event: any) => {
+    const { type, name, value, checked } = event.target;
 
-    const { type, name, value, checked } = event.target
+    let updatedValue = type === "checkbox" ? checked : value;
 
-    let updatedValue = type === "checkbox" ? checked: value
-
-    if(type === "number") {
-      updatedValue = Number(updatedValue)
+    if (type === "number") {
+      updatedValue = Number(updatedValue);
     }
 
     const change = {
-      [name]: updatedValue
+      [name]: updatedValue,
+    };
+
+    let updatedProject: Project;
+
+    setProject((p) => {
+      updatedProject = new Project({ ...p, ...change });
+      return updatedProject;
+    });
+
+    setErrors(() => validate(updatedProject));
+  };
+
+
+  function validate(project: Project) {
+    let errors: any = { name: "", description: "", budget: "" };
+
+    if (project.name.length === 0) {
+      errors.name = "Name is required";
     }
 
-    let updatedProject: Project
+    if (project.name.length > 0 && project.name.length < 3) {
+      errors.name = "Name needs to be at least 3 characters.";
+    }
 
-    setProject(p => {
-      updatedProject = new Project({ ...p, ...change })
-      return updatedProject
-    })
+    if (project.description.length === 0) {
+      errors.description = "Description is required.";
+    }
+
+    if (project.budget === 0) {
+      errors.budget = "Budget must be more than $0";
+    }
+
+    return errors;
   }
 
+
+  function isValid() {
+    return (
+      errors.name.length === 0 &&
+      errors.description.length === 0 &&
+      errors.budget.length === 0
+    );
+  }
+  
+
   return (
-    <form className="input-group vertical" onSubmit={handleSubmit}>
-      <label htmlFor="name">Project Name</label>
-      <input type="text" name="name" placeholder="enter name" value={project.name} onChange={handleChange}/>
 
-      <label htmlFor="description">Project Description</label>
-      <textarea name="description" placeholder="enter description" value={project.description} onChange={handleChange}></textarea>
+      <form className="input-group vertical" onSubmit={handleSubmit}>
+        <label htmlFor="name">Project Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="enter name"
+          value={project.name}
+          onChange={handleChange}
+        />
+        {errors.name.length > 0 && (
+        <div className="card error">
+          <p>{errors.name}</p>
+        </div>
+      )}
 
-      <label htmlFor="budget">Project Budget</label>
-      <input type="number" name="budget" placeholder="enter budget" value={project.budget} onChange={handleChange}/>
+        <label htmlFor="description">Project Description</label>
+        <textarea
+          name="description"
+          placeholder="enter description"
+          value={project.description}
+          onChange={handleChange}
+        ></textarea>
+        {errors.description.length > 0 && (
+          <div className="card error">
+          <p>{errors.description}</p>
+        </div>
+        )}
 
-      <label htmlFor="isActive">Active?</label>
-      <input type="checkbox" name="isActive" checked={project.isActive} onChange={handleChange}/>
+        <label htmlFor="budget">Project Budget</label>
+        <input
+          type="number"
+          name="budget"
+          placeholder="enter budget"
+          value={project.budget}
+          onChange={handleChange}
+        />
+        {errors.budget.length > 0 && (
+          <div className="card error">
+          <p>{errors.budget}</p>
+        </div>
+        )}
 
-      <div className="input-group">
-        <button className="primary bordered medium">Save</button>
-        <span></span>
-        <button type="button" className="bordered medium" onClick={onCancel}>
-          Cancel
-        </button>
-      </div>
-    </form>
+        <label htmlFor="isActive">Active?</label>
+        <input
+          type="checkbox"
+          name="isActive"
+          checked={project.isActive}
+          onChange={handleChange}
+        />
+
+        <div className="input-group">
+          <button className="primary bordered medium">Save</button>
+          <span></span>
+          <button type="button" className="bordered medium" onClick={onCancel}>
+            Cancel
+          </button>
+        </div>
+      </form>
+
+      
+ 
   );
 }
 
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,23 +1,95 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import ProjectList from "./ProjectList";
 import { MOCK_PROJECTS } from "./MockProjects";
 import { Project } from "./Project";
+import { projectAPI } from "./projectAPI";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  // const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(currentPage);
+        setError("");
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, [currentPage]);
 
   const saveProject = (project: Project) => {
-    let updatedProjects = projects.map((p: Project) => {
-      return p.id === project.id ? project : p;
-    });
-    setProjects(updatedProjects);
+    projectAPI
+    .put(project)
+    .then((updatedProject) => {
+      let updatedProjects = projects.map((p: Project) => {
+        return p.id === project.id ? new Project(updatedProject) : p
+      })
+      setProjects(updatedProjects)
+    })
+    .catch(e => {
+      if(e instanceof Error) {
+        setError(e.message)
+      }
+    })
   };
 
   return (
     <Fragment>
       <h1>ProjectsPage</h1>
       <ProjectList projects={projects} onSave={saveProject} />
+
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </Fragment>
   );
 }
